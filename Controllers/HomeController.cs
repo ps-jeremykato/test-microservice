99dd788759619bcadd40b7ec009e7e64752c67c5
Jeremy Kato <jeremy.kato@crisphealth.org>
2020-07-20 18:01:20 -0400

    Auth stuff

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Test.Controllers
@@ -11,6 +12,7 @@ namespace Test.Controllers
         }
 
         [HttpGet("/api/Test")]
+        [Authorize]
         public IActionResult Test()
         {
             return Ok(new { success = true });
