03467b50d65b5d397ed3a538ee968fc1cc533be1
Jeremy Kato <jeremy.kato@crisphealth.org>
2020-07-20 16:59:51 -0400

    Basic repo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+        }
+
+        [HttpGet("/api/Test")]
+        public IActionResult Test()
+        {
+            return Ok(new { success = true });
+        }
+    }
+}
